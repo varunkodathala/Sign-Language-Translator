d39368889814f048c5803437889183c42b4a5861
sai varun kodathala <51326090+varunkodathala@users.noreply.github.com>
2020-04-29 16:59:08 +0530

    Add files via upload

---
+++ display/display.m
@@ -0,0 +1,145 @@
+clc;
+close all;
+clear all;
+A = imread('A.jpg');
+B = imread('B.jpg');
+C = imread('C.jpg');
+D = imread('D.jpg');
+E = imread('E.jpg');
+F = imread('F.jpg');
+G = imread('G.jpg');
+H = imread('H.jpg');
+I = imread('I.jpg');
+J = imread('J.jpg');
+K = imread('K.jpg');
+L = imread('L.jpg');
+M = imread('M.jpg');
+N = imread('N.jpg');
+O = imread('O.jpg');
+P = imread('P.jpg');
+Q = imread('Q.jpg');
+R = imread('R.jpg');
+S = imread('S.jpg');
+T = imread('T.jpg');
+U = imread('U.jpg');
+V = imread('V.jpg');
+W = imread('W.jpg');
+X = imread('X.jpg');
+Y = imread('Y.jpg');
+Z = imread('Z.jpg');
+DEL = imread('del.jpg');
+space = imread('space.jpg');
+
+subplot(3,4,1)
+imshow(A)
+title('A')
+
+subplot(3,4,2)
+imshow(B)
+title('B')
+
+subplot(3,4,3)
+imshow(A)
+title('C')
+
+subplot(3,4,4)
+imshow(D)
+title('D')
+
+subplot(3,4,5)
+imshow(E)
+title('E')
+
+subplot(3,4,6)
+imshow(F)
+title('F')
+
+subplot(3,4,7)
+imshow(G)
+title('G')
+
+subplot(3,4,8)
+imshow(H)
+title('H')
+
+subplot(3,4,9)
+imshow(I)
+title('I')
+
+subplot(3,4,10)
+imshow(J)
+title('J')
+
+subplot(3,4,11)
+imshow(K)
+title('K')
+
+subplot(3,4,12)
+imshow(L)
+title('L')
+
+figure()
+
+subplot(4,4,1)
+imshow(M)
+title('M')
+
+subplot(4,4,2)
+imshow(N)
+title('N')
+
+subplot(4,4,3)
+imshow(O)
+title('O')
+
+subplot(4,4,4)
+imshow(P)
+title('P')
+
+subplot(4,4,5)
+imshow(Q)
+title('Q')
+
+subplot(4,4,6)
+imshow(R)
+title('R')
+
+subplot(4,4,7)
+imshow(S)
+title('S')
+
+subplot(4,4,8)
+imshow(T)
+title('T')
+
+subplot(4,4,9)
+imshow(U)
+title('U')
+
+subplot(4,4,10)
+imshow(V)
+title('V')
+
+subplot(4,4,11)
+imshow(W)
+title('W')
+
+subplot(4,4,12)
+imshow(X)
+title('X')
+
+subplot(4,4,13)
+imshow(Y)
+title('Y')
+
+subplot(4,4,14)
+imshow(Z)
+title('Z')
+
+subplot(4,4,15)
+imshow(DEL)
+title('del')
+
+subplot(4,4,16)
+imshow(space)
+title('space')
